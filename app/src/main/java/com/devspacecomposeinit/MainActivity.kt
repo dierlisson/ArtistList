d2f469c201e2e69a16aacdd0decaee3b30f485de
Dierlisson Justiniano <dierlisson1@gmail.com>
2025-05-15 22:36:52 -0300

    Utilizado Lasy Column

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -3,38 +3,37 @@ package com.devspacecomposeinit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.devspacecomposeinit.ui.theme.ComposeInitTheme
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import android.R.attr.alertDialogIcon
-import android.media.Image
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.nio.file.WatchEvent
+import com.devspacecomposeinit.ui.theme.ComposeInitTheme
+
+annotation class DrawableRes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,13 +45,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val artist = Artist(
+                    val leonardo = Artist(
                         name = "Alfred Sisley",
                         lastSeenOnLine = "3 minutes ago",
                         image = R.drawable.ic_leonardo_da_vinci,
                         art = R.drawable.ic_mona_lisa
                     )
-                    ArtistCard(artist)
+                    val picasso = Artist(
+                        name = "Pablo Picasso",
+                        lastSeenOnLine = "5 minutes ago",
+                        image = R.drawable.ic_pablo_picasso,
+                        art = R.drawable.ic_beijo
+                    )
+                    val salvador = Artist(
+                        name = "Salvador Dali",
+                        lastSeenOnLine = "10 minutes ago",
+                        image = R.drawable.ic_salvador_dali,
+                        art = R.drawable.ic_persistence_of_memory
+                    )
+                    val vanGogh = Artist(
+                        name = "Vincent Van Gogh",
+                        lastSeenOnLine = "7 minutes ago",
+                        image = R.drawable.ic_salvador_dali,
+                        art = R.drawable.ic_persistence_of_memory
+                    )
+                    val artists = listOf(vanGogh, salvador, picasso, leonardo)
+                    LazyColum{
+                        items(artists) { artist ->
+                            ArtistCard(
+                                artist = artist,
+                                onClick = {
+                                    println('rock teste' + artist.name)
+                                }
+                            )
                 }
             }
         }
@@ -60,9 +85,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArtistCard(artist: Artist) {
+fun ArtistCard(
+    artist: Artist,
+    onClick: () -> Unit = {}
+) {
     Column(
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
@@ -92,7 +122,8 @@ fun ArtistCard(artist: Artist) {
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
         ) {
             Image(
-                modifier= Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(200.dp),
                 contentScale = ContentScale.Crop,
                 painter = painterResource(id = artist.art),
@@ -122,6 +153,10 @@ private fun ArtistCardPreview() {
             image = R.drawable.ic_leonardo_da_vinci,
             art = R.drawable.ic_mona_lisa
         )
-        ArtistCard(artist)
+        ArtistCard(
+            artist = artist,
+            onClick = {
+
+            })
     }
 }
